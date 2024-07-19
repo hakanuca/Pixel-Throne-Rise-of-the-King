04efcc5d71f2e3fdc9fb21c4dd810ed605ead8db
HakanUca <hakanhuca@gmail.com>
2024-07-19 16:11:28 +0300

    Totally new ui

--- Pixel-Throne-Rise-of-the-King/Assets/Scripts/MainMenu.cs
+++ Pixel-Throne-Rise-of-the-King/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject targetObject; 
+    [SerializeField] private GameObject targetObject;
     [SerializeField] private RectTransform visiblePosition;
+
+    [SerializeField] private GameObject sub1;
+    [SerializeField] private RectTransform sub1Visible;
+
+    [SerializeField] private GameObject sub2;
+    [SerializeField] private RectTransform sub2Visible;
 
     [SerializeField] private GameObject menu1;
     [SerializeField] private RectTransform visiblePositionMenu1;
@@ -23,16 +29,24 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject menu5;
     [SerializeField] private RectTransform visiblePositionMenu5;
 
+    [SerializeField] private GameObject itchio;
+
+    [SerializeField] private GameObject gp;
+
     void Start()
     {
         LeanTween.delayedCall(1f, () => LeanTween.move(targetObject, visiblePosition.position, 1f));
-        LeanTween.delayedCall(2f, () => LeanTween.move(menu1, visiblePositionMenu1.position, 1f));
-        LeanTween.delayedCall(3f, () => LeanTween.move(menu2, visiblePositionMenu2.position, 1f));
-        LeanTween.delayedCall(4f, () => LeanTween.move(menu3, visiblePositionMenu3.position, 1f));
-        LeanTween.delayedCall(5f, () => LeanTween.move(menu4, visiblePositionMenu4.position, 1f));
-        LeanTween.delayedCall(6f, () => LeanTween.move(menu5, visiblePositionMenu5.position, 1f));
-    }
+        LeanTween.delayedCall(2f, () => LeanTween.move(sub1, sub1Visible.position, 1f));
+        LeanTween.delayedCall(2f, () => LeanTween.move(sub2, sub2Visible.position, 1f));
 
+        LeanTween.delayedCall(3f, () => LeanTween.move(menu1, visiblePositionMenu1.position, 1f));
+        LeanTween.delayedCall(4f, () => LeanTween.move(menu2, visiblePositionMenu2.position, 1f));
+        LeanTween.delayedCall(5f, () => LeanTween.move(menu3, visiblePositionMenu3.position, 1f));
+        LeanTween.delayedCall(6f, () => LeanTween.move(menu4, visiblePositionMenu4.position, 1f));
+        LeanTween.delayedCall(7f, () => LeanTween.move(menu5, visiblePositionMenu5.position, 1f));
+        LeanTween.delayedCall(8.5f, () => itchio.SetActive(true));
+        LeanTween.delayedCall(8.5f, () => gp.SetActive(true));
+    }
 
     public void PlayGame()
     {
