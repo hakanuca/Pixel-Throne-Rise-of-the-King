006a3152a125905d99fd1ed324bb45c2899ff5cc
HakanUca <hakanhuca@gmail.com>
2024-08-17 21:33:21 +0300

    new sfx base implemented

---
+++ Pixel-Throne-Rise-of-the-King/Assets/Scripts/PlaySoundOnIdle.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlaySoundOnIdle : MonoBehaviour
+{
+    public AudioSource runSound;  // Assign the AudioSource component with the run sound
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Check if the current animation state is "Main_Character_Run"
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Main_Character_Idle"))
+        {
+            // Play the sound if it's not already playing
+            if (!runSound.isPlaying)
+            {
+                runSound.Play();
+            }
+        }
+        else
+        {
+            // Stop the sound if the animation state changes
+            if (runSound.isPlaying)
+            {
+                runSound.Stop();
+            }
+        }
+    }
+}
