146da77a25346e352135d07eb4b3fb3d563d6e9c
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2024-09-16 19:10:22 +0300

    Created Boss Scenes And Fixed Final Boss Laser Bug

--- Pixel-Throne-Rise-of-the-King/Assets/Scripts/Laser.cs
+++ Pixel-Throne-Rise-of-the-King/Assets/Scripts/Laser.cs
@@ -1,26 +1,35 @@
+using System.Collections;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     private Collider2D laserCollider;
+    [SerializeField] private float colliderDelay = 3f;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         laserCollider = GetComponent<Collider2D>();
+        laserCollider.enabled = false;
     }
 
     public void Activate()
     {
-        laserCollider.enabled = true;
         laserCollider.gameObject.layer = LayerMask.NameToLayer("Player");
         animator.SetBool("isAttacking", true);
+        StartCoroutine(EnableColliderAfterDelay(colliderDelay));
     }
 
     public void Deactivate()
     {
         laserCollider.enabled = false;
         animator.SetBool("isAttacking", false);
+    }
+
+    private IEnumerator EnableColliderAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        laserCollider.enabled = true;
     }
 }
